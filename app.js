ce8ea4d2c2d377cd7cf1133005c2543d081758cc
BheshRajNeupane <108607897+BheshRajNeupane@users.noreply.github.com>
2023-12-09 14:30:58 +0545

    All testing pass  ,add some security

--- app.js
+++ app.js
@@ -3,21 +3,67 @@ const app = express();
 const userRouter = require('./routes/usersRoutes');
 const movieRouter = require('./routes/movieRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
-const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const globalErrorHandler = require('./controllers/errorController');
 
-// app.use((req, res, next)=>{
-//     console.log("testing..!!!!");
-//     next();
-// })
-// app.get('/', (req, res)=>{
-//     res.status(200)
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize') 
+const xss = require('xss-clean');
+const hpp = require('hpp'); 
 
-// })
+
+// 1)Global Middleware
+
+// to Serve static file
+//app.use(express.static(path.join( __dirname, 'public')));
+
+//Set security HTTP headers .
+app.use(helmet());
+
+//Development Logging
+if(process.env.NODE_ENV ==='development'){
+app.use(morgan('dev'))
+}
+
+
+// Implementing Rate Limiting
+const limiter =  rateLimit({ //pass to rateLimit fxn
+ max:100,
+ windowMs:60 * 60 * 1000,
+ message:'To many request from this IP , please try again in hour'   
+})
+app.use( '/api',limiter);
+
 
 app.use(express.json({limit:'10kb'}));
 app.use(cookieParser())
+
+
+//Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+
+//Data sanitization against XSS
+app.use(xss());
+ 
+
+//Prevent parameter pollution
+app.use(
+    hpp({
+      whitelist:[
+       'title',
+       'director',
+       'casts',
+       'showTimes',
+       'rRating',
+       'Price',
+       "script"
+    ]
+}))
+
+
 
 //Routes
 app.use('/api/users' ,userRouter);
@@ -31,7 +77,7 @@ app.all( ' * ' , (req,res,next)=>{
     next( new AppError(`Can't find ${req.originalUrl} on this server!`,404));
    });
 
- 
+ //GloableErrorHandling
  app.use(globalErrorHandler);
 
 module.exports = app;
